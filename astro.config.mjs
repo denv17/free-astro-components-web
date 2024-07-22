1044d9d32ba5c1091854e21e259ab41cde8cfe10
Multiplica <denis@MacBook-Pro-de-Multiplica.local>
2024-07-21 23:38:11 -0500

    add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
+  integrations: [tailwind()],
+})
