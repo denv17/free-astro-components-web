04986f84e7d6dfd285d93083059621f36ef41bf1
Denis Ventura <dventura017@email.com>
2024-08-03 17:59:22 -0500

    add installation guide

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,24 @@
 import { defineConfig } from 'astro/config'
-
 import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel/serverless'
+import astroExpressiveCode from 'astro-expressive-code'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel({
-    webAnalytics: { enabled: true },
+    webAnalytics: {
+      enabled: true,
+    },
   }),
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    astroExpressiveCode({
+      themes: ['poimandres', 'light-plus'],
+      styleOverrides: {
+        borderRadius: '0.5rem',
+        borderWidth: '0px',
+      },
+    }),
+  ],
 })
