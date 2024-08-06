63d82f911c82892a2171e6dbdca5c4d64b6b9318
Denis Ventura <dventura017@email.com>
2024-08-06 14:31:53 -0500

    add tab component page

---
+++ src/data/tab.ts
@@ -0,0 +1,50 @@
+export const importCode = `
+---
+import { Tab, TabItem } from 'free-astro-components'
+---
+`
+export const basicUsageCode = `
+---
+import { Tab, TabItem } from 'free-astro-components'
+---
+
+<Tab
+  tabs={[{ label: 'Tab 1', active: true }, { label: 'Tab 2' }]}
+>
+  <TabItem active>Content for Tab 1</TabItem>
+  <TabItem>Content for Tab 2</TabItem>
+</Tab>
+`
+
+export const tableTabHead = ['Attribute', 'Type', 'Description', 'Default']
+
+export const tableTabRows = [
+  [
+    { head: 'Attribute', label: 'tabs' },
+    {
+      head: 'Type',
+      label: '<code>{ label: string, active?: boolean }[]</code>',
+    },
+    {
+      head: 'Description',
+      label:
+        'An array of objects defining the tabs. Each object should have a <code>label</code> for the tab title and an optional <code>active</code> boolean to indicate the active tab.',
+    },
+    {
+      head: 'Default',
+      label:
+        "<code>[{ label: 'tab 1', active: true }, { label: 'tab 2' }]</code>",
+    },
+  ],
+]
+
+export const tableTabItemHead = ['Attribute', 'Type', 'Description', 'Default']
+
+export const tableTabItemRows = [
+  [
+    { head: 'Attribute', label: 'active' },
+    { head: 'Type', label: '<code>boolean</code>' },
+    { head: 'Description', label: 'Indicates whether the tab is active.' },
+    { head: 'Default', label: '<code>false</code>' },
+  ],
+]
